ca6622bbeafb685dabd69fe2046da26f32e9564a
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-03-20 16:10:49 +0100

    Add files via upload

---
+++ PerimeterAndAreaCalculator.py
@@ -0,0 +1,8 @@
+base = float(input("Enter the base: "))    
+height = float(input("Enter the Height: "))
+
+perimeter = (base + height) * 2;
+area = base * height;
+
+print("The measure of the perimeter is ", perimeter)
+print("The measure of the area is ", area)
