feef66c3718adaf6197729347382efaa9ccf8422
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-04-05 19:42:05 +0200

    Update QR-Code Creator.py

--- QR-Code Creator.py
+++ QR-Code Creator.py
@@ -1,9 +1,7 @@
-import pyqrcode
-
+#pip install PyQRCode or go to https://pypi.org/project/PyQRCode/#files
 value = str(input("Enter the value of the QR-Code: "))
 
 qr = pyqrcode.create(value)
-
 qr.svg("qrcode.svg", scale = 8)
 
 stop = str(input("QR-Code Created Successfully"))
