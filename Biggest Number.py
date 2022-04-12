59dec5e642bedb31d960484acaf0bf0ac6c6eabb
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-04-12 21:41:20 +0200

    Update Biggest Number.py

--- Biggest Number.py
+++ Biggest Number.py
@@ -1,16 +1,8 @@
-num1=int(input("Enter the first Number"))   #output and input
-num2=int(input("Enter the second Number"))
-num3=int(input("Enter the third Number"))
-num4=int(input("Enter the fourth Number"))
-num5=int(input("Enter the fifth Number"))
-num6=int(input("Enter the sixth Number"))
-num7=int(input("Enter the seventh Number"))
-num8=int(input("Enter the eighth Number"))
-num9=int(input("Enter the ninth Number"))
-num10=int(input("Enter the tenth Number"))
+maxnum = int(0)
 
-num=[num1, num2, num3, num4, num5, num6, num7, num8, num9, num10] #array
+for i in range(10):
+    num = int(input("Enter the number: "))
+    if maxnum < num:
+        maxnum = num
 
-num11=max(num) #create a new variable that is the biggest number
-
-print("The biggest Number is " , num11) #output
+print("The biggest number is ", maxnum)
