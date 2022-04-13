45a4b7d8de7dd522ec29abe109b83c724a0f89c5
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-04-13 21:59:11 +0200

    Delete PerimeterAndAreaCalculator.py

--- PerimeterAndAreaCalculator.py
+++
@@ -1,8 +0,0 @@
-base = float(input("Enter the base: "))    
-height = float(input("Enter the Height: "))
-
-perimeter = (base + height) * 2;
-area = base * height;
-
-print("The measure of the perimeter is ", perimeter)
-print("The measure of the area is ", area)
