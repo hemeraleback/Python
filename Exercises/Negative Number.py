7529361f8227c00dd813eeefc0ed5cfd3be29468
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-04-13 21:58:49 +0200

    Add files via upload

---
+++ Exercises/Negative Number.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the number you want to know if is positive or negative: "))
+
+if num < 0:
+    print("Your Number is negative")
+elif num > 0:
+    print("Your Number is positive")
+elif num == 0:
+    print("Your Number is not positive and not negative because is equal to zero")
