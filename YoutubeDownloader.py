14988cdb3902885dcb3b2d8ecc316dfba5695733
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-06-25 21:11:57 +0200

    Update YoutubeDownloader.py

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -4,7 +4,7 @@ from pytube import YouTube
 url = str(input("Enter the link: "))
 video = YouTube(url)
 
-print("Downloading :", video.title)
+print("Downloading : ", video.title)
 print("Thumbnail : ", video.thumbnail_url)
 
 video = video.streams.get_highest_resolution()
