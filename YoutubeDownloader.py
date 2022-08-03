d5303d8d840c02ba420e55fe43af5ec367815d66
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-08-03 17:42:13 +0200

    Update YoutubeDownloader.py

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -1,8 +1,7 @@
 #pip install pytube, or go to https://github.com/pytube/pytube.git
 from pytube import YouTube
 
-url = str(input("Enter the link: "))
-video = YouTube(url)
+video = YouTube(str(input("Enter the link of the video: ")))
 
 print("Downloading : ", video.title)
 print("Thumbnail : ", video.thumbnail_url)
@@ -11,4 +10,3 @@ video = video.streams.get_highest_resolution()
 video.download()
 
 print("Downloaded Succesfully")
-
