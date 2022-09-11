298795c7dfed2086f34ee6652452ec3cc475f83d
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-09-11 16:17:27 +0200

    Update Rest.py

--- Exercises/Rest.py
+++ Exercises/Rest.py
@@ -1,10 +1,7 @@
 money = float(input("Enter how much money you have: "))
 cost = float(input("Enter how much the product cost: "))
 
-poor=float(cost-money)
-rest=float(money-cost)
-
 if cost>money:
-    print("You don't have enough money, you are missing ", poor, " euros ")
+    print("You don't have enough money, you are missing ", cost-money , " euros")
 else:
-    print("You can buy this product, and you are missing ", rest, "euros")
+    print("You can buy this product, and you are missing ", money-cost, "euros")
